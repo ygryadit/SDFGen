4ee918005fa36b2d64c028e074386956eb39c8a6
julia.poste@gmail.com <julia.poste@gmail.com>
2021-04-30 15:36:53 +0100

    Changes to have different input parameters.

--- main.cpp
+++ main.cpp
@@ -20,9 +20,33 @@
 #include <sstream>
 #include <limits>
 
+void rescaleTheShape(const Vec3f min_box, const Vec3f max_box, 
+                    float min_box_dim, float max_box_dim, 
+                    std::vector<Vec3f>& vertList)
+{
+    // Keep a small margin from min_box_dim and max_box_dim
+    float padded_dim = 1e-5 * ((double)max_box_dim - (double)min_box_dim);
+
+    Vec3f center = (max_box + min_box) / 2.0;
+    
+    std::cout << "center = " << center << std::endl;
+
+    float max_dim = max(max_box - min_box);
+
+    double scale = padded_dim / max_dim;
+    std::cout << "scale = " << scale << std::endl;
+
+    for (unsigned int t = 0; t < vertList.size(); ++t) {
+        //center
+        std::cout << "vert = " << vertList[t] << std::endl;
+        vertList[t] = (vertList[t] - center) * scale;
+        std::cout << "vertScaled = " << vertList[t] << std::endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
   
-  /*if(argc != 4) {
+  
     std::cout << "SDFGen - A utility for converting closed oriented triangle meshes into grid-based signed distance fields.\n";
     std::cout << "\nThe output file format is:";
     std::cout << "<ni> <nj> <nk>\n";
@@ -40,41 +64,52 @@ int main(int argc, char* argv[]) {
     std::cout << "Usage: SDFGen <filename> <dx> <padding>\n\n";
     std::cout << "Where:\n";
     std::cout << "\t<filename> specifies a Wavefront OBJ (text) file representing a *triangle* mesh (no quad or poly meshes allowed). File must use the suffix \".obj\".\n";
-    std::cout << "\t<dx> specifies the length of grid cell in the resulting distance field.\n";
-    std::cout << "\t<padding> specifies the number of cells worth of padding between the object bound box and the boundary of the distance field grid. Minimum is 1.\n\n";
+    std::cout << "\t<min_box_dim> specifies the minimum dimension of the grid bounding box.\n";
+    std::cout << "\t<max_box_dim> specifies the maximum dimension of the grid bounding box.\n";    
+    std::cout << "\t<grid_size> specifies the maximum dimension of the grid bounding box.\n";
     
-    exit(-1);
-  }*/
+    if (argc != 5) {
+        exit(-1);
+    }
 
-  std::string filename(argv[1]);
-  if(filename.size() < 5 || filename.substr(filename.size()-4) != std::string(".obj")) {
-    std::cerr << "Error: Expected OBJ file with filename of the form <name>.obj.\n";
-    exit(-1);
-  }
+    //Check that the first file is in 'obj' format.
+    std::string filename(argv[1]);
+    if(filename.size() < 5 || filename.substr(filename.size()-4) != std::string(".obj")) {
+        std::cerr << "Error: Expected OBJ file with filename of the form <name>.obj.\n";
+        exit(-1);
+    }
 
-  //std::stringstream arg2(argv[2]);
-  /*float dx;
-  arg2 >> dx;*/
+    //Read bounding box dimensions:
+    std::stringstream arg2(argv[2]);
+    float min_box_dim;
+    arg2 >> min_box_dim;
+
+    std::stringstream arg3(argv[3]);
+    float max_box_dim;
+    arg3 >> max_box_dim;
+
+
+    std::stringstream arg4(argv[4]);
+    float grid_size;
+    arg4 >> grid_size;
+    
+    
+
 
     float dx;
      
+    
+    //The varaiable for the object actual bounding box:
+    Vec3f   min_box(std::numeric_limits<float>::max(),std::numeric_limits<float>::max(),std::numeric_limits<float>::max()), 
+            max_box(-std::numeric_limits<float>::max(),-std::numeric_limits<float>::max(),-std::numeric_limits<float>::max());
   
-  //std::stringstream arg3(argv[3]);
-  //int padding;
-  //arg3 >> padding;
+    std::cout << "Reading data.\n";
 
-  //if(padding < 1) padding = 1;
-  //start with a massive inside out bound box.
-  Vec3f min_box(std::numeric_limits<float>::max(),std::numeric_limits<float>::max(),std::numeric_limits<float>::max()), 
-    max_box(-std::numeric_limits<float>::max(),-std::numeric_limits<float>::max(),-std::numeric_limits<float>::max());
-  
-  std::cout << "Reading data.\n";
-
-  std::ifstream infile(argv[1]);
-  if(!infile) {
-    std::cerr << "Failed to open. Terminating.\n";
-    exit(-1);
-  }
+    std::ifstream infile(argv[1]);
+    if(!infile) {
+        std::cerr << "Failed to open. Terminating.\n";
+        exit(-1);
+    }
 
   int ignored_lines = 0;
   std::string line;
@@ -99,10 +134,14 @@ int main(int argc, char* argv[]) {
       data >> c >> v0 >> v1 >> v2;
       faceList.push_back(Vec3ui(v0-1,v1-1,v2-1));
     }
-    //else if( line.substr(0,2) == std::string("vn") ){
-    //  std::cerr << "Obj-loader is not able to parse vertex normals, please strip them from the input file. \n";
-    //  exit(-2); 
-    //}
+    else if( line.substr(0,2) == std::string("vn") ){
+      std::cerr << "Obj-loader is not able to parse vertex normals, please strip them from the input file. \n";
+      exit(-2); 
+    }
+    else if (line.substr(0, 2) == std::string("vt")) {
+        std::cerr << "Obj-loader is not able to parse vertex textures, please strip them from the input file. \n";
+        exit(-2);
+    }
     else {
       ++ignored_lines; 
     }
@@ -110,21 +149,30 @@ int main(int argc, char* argv[]) {
   infile.close();
   
   if(ignored_lines > 0)
-    std::cout << "Warning: " << ignored_lines << " lines were ignored since they did not contain faces or vertices.\n";
+        std::cout << "Warning: " << ignored_lines << " lines were ignored since they did not contain faces or vertices.\n";
 
   std::cout << "Read in " << vertList.size() << " vertices and " << faceList.size() << " faces." << std::endl;
 
-  std::cout << "Bound box size: (" << min_box << ") to (" << max_box << ") dim sum = " << max_box - min_box << std::endl;
+  std::cout << "Shape bound box size: (" << min_box << ") to (" << max_box << ") dim sum = " << max_box - min_box << std::endl;
+
+  
+
+
 
 
   //Add padding around the box.
   //Vec3f unit(1,1,1);
-  Vec3f min_box_(-0.8, -0.8, -0.8);
-  Vec3f max_box_(0.8, 0.8, 0.8);
+  Vec3f min_box_(min_box_dim, min_box_dim, min_box_dim);
+  Vec3f max_box_(max_box_dim, max_box_dim, max_box_dim);
 
-  if ((min_box[0] < min_box_[0]) | (min_box[1] < min_box_[1]) | (min_box[2] < min_box_[2]) |
+  rescaleTheShape(min_box, max_box,
+      min_box_dim, max_box_dim,
+      vertList);
+
+
+  /*if ((min_box[0] < min_box_[0]) | (min_box[1] < min_box_[1]) | (min_box[2] < min_box_[2]) |
       (max_box[0] > max_box_[0]) | (max_box[1] > max_box_[1]) | (max_box[2] < max_box_[2]))
-      std::cerr << "The shape is assumed to be normalised to have the digonal equal to 1" << std::endl;
+      std::cerr << "The shape is assumed to be normalised to have the digonal equal to 1" << std::endl;*/
 
   /*min_box -= padding*dx*unit;
   max_box += padding*dx*unit;*/
@@ -136,7 +184,7 @@ int main(int argc, char* argv[]) {
 
   /*Vec3ui sizes = Vec3ui((max_box - min_box)/dx);*/
   
-  Vec3ui sizes(64, 64, 64);
+  Vec3ui sizes(grid_size, grid_size, grid_size);
 
   dx = (max_box[0] - min_box[0]) / sizes[0];
   
